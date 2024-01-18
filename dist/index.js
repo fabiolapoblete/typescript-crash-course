7954d545c6d6e1c4dabbb94d9baba9252d92fc81
Fabiola Poblete <fabiola.broccolo@gmail.com>
2024-01-18 11:50:26 +0100

    Arrays and object literals

--- dist/index.js
+++ dist/index.js
@@ -6,4 +6,29 @@
 // In order to avoid writing tsc each time we want to compile our files we can write tsc --watch
 // This will automatically compile our files whenever we hit save
 // We can do the same with node, it will then watch for changes in the index.js file. Write node --watch dist/index.js (in this case)
-console.log("hello world");
+/*ARRAYS*/
+let names = ['Mario', 'Luigi', 'Peach']; //A list of strings
+let ages = [25, 28, 24]; //A list of numbers
+names.push('Bowser');
+ages.push(5);
+/*TYPE INTERFERENCE WITH ARRAYS*/
+//JS will understand that this is a string array
+let fruits = ['apples', 'pears', 'bananas', 'mango'];
+fruits.push('peaches');
+let things = [1, true, 'hello'];
+//Now this can be either one of theese types (number, boolean, string) a "union type"
+const t = things[0]; //TS still says it can be any of theese types although we know it is a number
+/*OBJECT LITERALS*/
+let user = {
+    firstName: 'Mario',
+    age: 30,
+    id: 1
+};
+//The user is of type object with contants a couple of properties of different types
+// The object must also include exaclty the properties assigned in the beginning. We have to stick to the structure.
+// We can only change the values but not add a new property.
+/*TYPE INTERFERENCE WITH OBJECT LITERALS*/
+let person = {
+    name: 'luigi',
+    score: 35
+};
